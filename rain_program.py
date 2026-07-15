08d7206c9033bc6ffa3b69502b6a89f31351d546
mmalla1 <mmalla1@ivytech.edu>
2026-07-15 11:56:55 -0500

    Add files via upload

---
+++ rain_program.py
@@ -0,0 +1,4 @@
+c="cats" #This assigns the variable as c to cats
+d="dogs"  #This assigns the variable to d to dogs
+s="It is raining " + c + " and " + d +"." #This combines the variable c and variable d into one sentence to make a full sentence.
+print(s) #This prints the variable s which is the whole sentnece.
