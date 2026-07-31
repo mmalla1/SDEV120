042536c2ce2a2e840bcd6a48906d8fc2350330d7
mmalla1 <mmalla1@ivytech.edu>
2026-07-31 12:01:45 -0500

    Add files via upload
    Recent Functions in python code

---
+++ Functions in Python code.py
@@ -0,0 +1,10 @@
+def greater_than(x,y):
+    if x>y:
+        return True
+    else:
+        return False
+a=10
+b=6
+result=greater_than(a,b)
+print("This statement " + str(a) + " is greater than " + str(b) + " is "+ str(result))
+    
